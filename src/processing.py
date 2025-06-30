64ebae057d0f42c3da36c63efcf315ffd47072fd
shure0407 <eun.udm@gmail.com>
2025-06-30 05:18:44 +0400

    10.1_hw def sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -12,3 +12,18 @@ def filter_by_state(transactions, state):
                 executed_transactions.append(transaction)
 
     return executed_transactions
+
+
+def sort_by_date(transactions, sort):
+    """ Функция принимает список операций по карте и
+        возвращает список с сортировкой по дате """
+
+    if sort == "ascending_sort":
+        sorted_transactions_as = sorted(transactions, key=lambda transaction: transaction['date'])
+        return sorted_transactions_as
+    elif sort == "descending_sort":
+        sorted_transactions_des = sorted(transactions, key=lambda transaction: transaction['date'], reverse=True)
+        return sorted_transactions_des
+    else:
+        sorted_transactions_def = sorted(transactions, key=lambda transaction: transaction['date'], reverse=True)
+        return sorted_transactions_def

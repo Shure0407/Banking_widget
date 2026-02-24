84031b78dceb763b28195fec2e56d1e9ce14948e
shure0407 <eun.udm@gmail.com>
2026-02-25 02:48:23 +0400

    12_2_hw logging;+ edit decorators.py, main.py, test_decorators.py, readme.md

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-from src import widget
+from src import masks
 
-account_details = ()
-time_event = ()
+# from src import widget
 
-widget.mask_account_card(account_details)
-widget.get_date(time_event)
+# from src import processing
+
+# from src import generators
+
+# from src import decorators
+
+from src import utils
+
+# from src import external_api
+
+# import os
+
+# import requests
+
+
+account_or_card = input('''Введите номер карты или счета: ''')
+
+if len(account_or_card) == 16 and account_or_card.isdigit():
+    card_num = masks.get_mask_card_number(account_or_card)
+    print(card_num)
+elif len(account_or_card) == 20 and account_or_card.isdigit():
+    card_acc = masks.get_mask_account(account_or_card)
+    print(card_acc)
+else:
+    masks.get_mask_card_number(account_or_card)
+    masks.get_mask_account(account_or_card)
+
+data_tr = utils.load_transactions('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operation.json') # полный файл
+# data_tr = utils.load_transactions('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/123.json') # файл с тремя транзакциями
+# data_tr = utils.load_transactions('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/11.json') # файл не содержит список
+# data_tr = utils.load_transactions("C:/Users/Aleksandr/PycharmProjects/PythonProject/data/1.json") # пустой файл
+# print(data_tr)
+
+# for data in data_tr:
+#      result_conv = external_api.conversion(data)
+#      print(result_conv)
--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,14 @@
+import logging
 from typing import Any
+
+masks_logger = logging.getLogger(__name__)
+
+file_handler = logging.FileHandler("logs/masks.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
+
+masks_logger.addHandler(file_handler)
+file_handler.setFormatter(file_formatter)
+masks_logger.setLevel(logging.DEBUG)
 
 
 def get_mask_card_number(account_or_card: Any) -> Any:
@@ -6,8 +16,10 @@ def get_mask_card_number(account_or_card: Any) -> Any:
 
     if len(account_or_card) == 16 and account_or_card.isdigit():
         card_number = f"{account_or_card[0:4]} {account_or_card[4:6]}** **** {account_or_card[-4:]}"
+        masks_logger.info("Выдача номера карты с маской")
         return card_number
     else:
+        masks_logger.error("Неправильно введен номер карты")
         return print("Неправильно набран номер карты")
 
 
@@ -16,6 +28,8 @@ def get_mask_account(account_or_card: Any) -> Any:
 
     if len(account_or_card) == 20 and account_or_card.isdigit():
         account_number = f"**{account_or_card[-4:]}"
+        masks_logger.info("Выдача счета карты с маской")
         return account_number
     else:
+        masks_logger.error("Неправильно введен номер счета")
         return print("Неправильно набран номер счета")

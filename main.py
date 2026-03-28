d6d837708e96be0df2710df9bd9a67840a866675
shure0407 <eun.udm@gmail.com>
2026-03-28 21:08:32 +0400

    13_1_hw pandas; edit utils.py, main.py, test_utils.py, edit json's files names

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src import masks
+# from src import masks
 
 # from src import widget
 
@@ -8,33 +8,47 @@ from src import masks
 
 # from src import decorators
 
-from src import utils
+# from src import utils
 
 # from src import external_api
+
+from src import tables
 
 # import os
 
 # import requests
 
 
-account_or_card = input('''Введите номер карты или счета: ''')
+# account_or_card = input('''Введите номер карты или счета: ''')
 
-if len(account_or_card) == 16 and account_or_card.isdigit():
-    card_num = masks.get_mask_card_number(account_or_card)
-    print(card_num)
-elif len(account_or_card) == 20 and account_or_card.isdigit():
-    card_acc = masks.get_mask_account(account_or_card)
-    print(card_acc)
-else:
-    masks.get_mask_card_number(account_or_card)
-    masks.get_mask_account(account_or_card)
+# if len(account_or_card) == 16 and account_or_card.isdigit():
+#     card_num = masks.get_mask_card_number(account_or_card)
+#     print(card_num)
+# elif len(account_or_card) == 20 and account_or_card.isdigit():
+#     card_acc = masks.get_mask_account(account_or_card)
+#     print(card_acc)
+# else:
+#     masks.get_mask_card_number(account_or_card)
+#     masks.get_mask_account(account_or_card)
+#
+# data_json = utils.load_transactions('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operation.json') # полный файл
+# data_json = utils.load_transactions('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operations_three_trans.json') # файл с тремя транзакциями
+# data_json = utils.load_transactions('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operations_not_list.json') # файл не содержит список
+# data_json = utils.load_transactions("C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operations_empty.json") # пустой файл
+# print(data_json)
 
-data_tr = utils.load_transactions('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operation.json') # полный файл
-# data_tr = utils.load_transactions('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/123.json') # файл с тремя транзакциями
-# data_tr = utils.load_transactions('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/11.json') # файл не содержит список
-# data_tr = utils.load_transactions("C:/Users/Aleksandr/PycharmProjects/PythonProject/data/1.json") # пустой файл
-# print(data_tr)
-
-# for data in data_tr:
+# for data in data_json:
 #      result_conv = external_api.conversion(data)
 #      print(result_conv)
+
+# data_csv = tables.tables_csv('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions.csv')
+# data_csv = tables.tables_csv('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_three_str.csv')
+# data_csv = tables.tables_csv('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_not_list.csv')
+# data_csv = tables.tables_csv('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_empty.csv') # пустой файл
+# print(data_csv)
+
+# data_excel = tables.tables_excel('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_excel.xlsx')
+data_excel = tables.tables_excel('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_excel_three_str.xlsx')
+# data_excel = tables.tables_excel('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_not_list.xlsx')
+# data_excel = tables.tables_excel('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_excel_empty.xlsx') # пустой файл
+print(data_excel)
---
+++ tests/test_tables.py
@@ -0,0 +1,163 @@
+﻿from unittest.mock import patch
+
+from unittest.mock import Mock
+
+from src.tables import tables_csv
+
+from src.tables import tables_excel
+
+
+@patch("csv.DictReader")
+def test_tables_csv(mock_read_csv):
+    """Тестирование функции tables с помощью декоратора patch"""
+
+    mock_read_csv.return_value = [
+        {
+            "id": "650703",
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": "16210",
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
+        },
+        {
+            "id": "3598919",
+            "state": "EXECUTED",
+            "date": "2020-12-06T23:00:58Z",
+            "amount": "29740",
+            "currency_name": "Peso",
+            "currency_code": "COP",
+            "from": "Discover 3172601889670065",
+            "to": "Discover 0720428384694643",
+            "description": "Перевод с карты на карту",
+        },
+        {
+            "id": "593027",
+            "state": "CANCELED",
+            "date": "2023-07-22T05:02:01Z",
+            "amount": "30368",
+            "currency_name": "Shilling",
+            "currency_code": "TZS",
+            "from": "Visa 1959232722494097",
+            "to": "Visa 6804119550473710",
+            "description": "Перевод с карты на карту",
+        },
+    ]
+    assert tables_csv("C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_three_str.csv") == [
+        {
+            "id": "650703",
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": "16210",
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
+        },
+        {
+            "id": "3598919",
+            "state": "EXECUTED",
+            "date": "2020-12-06T23:00:58Z",
+            "amount": "29740",
+            "currency_name": "Peso",
+            "currency_code": "COP",
+            "from": "Discover 3172601889670065",
+            "to": "Discover 0720428384694643",
+            "description": "Перевод с карты на карту",
+        },
+        {
+            "id": "593027",
+            "state": "CANCELED",
+            "date": "2023-07-22T05:02:01Z",
+            "amount": "30368",
+            "currency_name": "Shilling",
+            "currency_code": "TZS",
+            "from": "Visa 1959232722494097",
+            "to": "Visa 6804119550473710",
+            "description": "Перевод с карты на карту",
+        },
+    ]
+
+
+def test_tables_excel():
+    """Тестирование функции tables_excel с помощью декоратора patch"""
+
+    mock_read_excel = Mock(
+        return_value=[
+            {
+                "id": 650703,
+                "state": "EXECUTED",
+                "date": "2023-09-05T11:30:32Z",
+                "amount": 16210,
+                "currency_name": "Sol",
+                "currency_code": "PEN",
+                "from": "Счет 58803664561298323391",
+                "to": "Счет 39745660563456619397",
+                "description": "Перевод организации",
+            },
+            {
+                "id": 3598919,
+                "state": "EXECUTED",
+                "date": "2020-12-06T23:00:58Z",
+                "amount": 29740,
+                "currency_name": "Peso",
+                "currency_code": "COP",
+                "from": "Discover 3172601889670065",
+                "to": "Discover 0720428384694643",
+                "description": "Перевод с карты на карту",
+            },
+            {
+                "id": 593027,
+                "state": "CANCELED",
+                "date": "2023-07-22T05:02:01Z",
+                "amount": 30368,
+                "currency_name": "Shilling",
+                "currency_code": "TZS",
+                "from": "Visa 1959232722494097",
+                "to": "Visa 6804119550473710",
+                "description": "Перевод с карты на карту",
+            },
+        ]
+    )
+
+    data_ex = mock_read_excel
+
+    assert tables_excel("C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_excel_three_str.xlsx") == [
+        {
+            "id": 650703,
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": 16210,
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
+        },
+        {
+            "id": 3598919,
+            "state": "EXECUTED",
+            "date": "2020-12-06T23:00:58Z",
+            "amount": 29740,
+            "currency_name": "Peso",
+            "currency_code": "COP",
+            "from": "Discover 3172601889670065",
+            "to": "Discover 0720428384694643",
+            "description": "Перевод с карты на карту",
+        },
+        {
+            "id": 593027,
+            "state": "CANCELED",
+            "date": "2023-07-22T05:02:01Z",
+            "amount": 30368,
+            "currency_name": "Shilling",
+            "currency_code": "TZS",
+            "from": "Visa 1959232722494097",
+            "to": "Visa 6804119550473710",
+            "description": "Перевод с карты на карту",
+        },
+    ]

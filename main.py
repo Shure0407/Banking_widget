b06d3ff6a4e1b4f8271f57ed3086c20dabde4a5d
Aleksandr Neklyudov <162669909+Shure0407@users.noreply.github.com>
2026-03-29 11:05:53 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -32,49 +32,35 @@ from src import tables
 #     masks.get_mask_account(account_or_card)
 #
 # data_json = utils.load_transactions(
-#   'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operation.json') # полный файл
+# 'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operation.json') # полный файл
 # data_json = utils.load_transactions(
-#   'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operations_three_trans.json') # файл с тремя транзакциями
+# 'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operations_three_trans.json') # файл с тремя транзакциями
 # data_json = utils.load_transactions(
-#   'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operations_not_list.json') # файл не содержит список
-# data_json = utils.load_transactions("
-#   C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operations_empty.json") # пустой файл
+# 'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operations_not_list.json') # файл не содержит список
+# data_json = utils.load_transactions(
+# 'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/operations_empty.json') # пустой файл
 # print(data_json)
 
 # for data in data_json:
 #      result_conv = external_api.conversion(data)
 #      print(result_conv)
 
-# data_csv = tables.tables_csv('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions.csv')
-# data_csv = tables.tables_csv('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_three_str.csv')
-# data_csv = tables.tables_csv('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_not_list.csv')
 # data_csv = tables.tables_csv(
-#   'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_empty.csv') # пустой файл
+# 'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions.csv')
+# data_csv = tables.tables_csv(
+# 'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_three_str.csv')
+# data_csv = tables.tables_csv(
+# 'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_not_list.csv')
+# data_csv = tables.tables_csv(
+# 'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_empty.csv') # пустой файл
 # print(data_csv)
 
-# data_excel = tables.tables_excel('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_excel.xlsx')
+# data_excel = tables.tables_excel(
+# 'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_excel.xlsx')
 data_excel = tables.tables_excel(
-    "C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_excel_three_str.xlsx"
-)
+    'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_excel_three_str.xlsx')
 # data_excel = tables.tables_excel(
-#   'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_not_list.xlsx')
+# 'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_not_list.xlsx')
 # data_excel = tables.tables_excel(
-#   'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_excel_empty.xlsx') # пустой файл
+# 'C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_excel_empty.xlsx') # пустой файл
 print(data_excel)
-data_excel = tables.tables_excel('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_excel_three_str.xlsx')
-# data_excel = tables.tables_excel('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_not_list.xlsx')
-# data_excel = tables.tables_excel('C:/Users/Aleksandr/PycharmProjects/PythonProject/data/transactions_excel_empty.xlsx') # пустой файл
-print(data_excel)
-account_details = str()
-time_event = str()
-
-widget.mask_account_card(account_details)
-widget.get_date(time_event)
-
-transactions: list[dict] = []
-state = ()
-sort = ()
-
-processing.filter_by_state(transactions, state)
-processing.sort_by_date(transactions, sort)
-

6a963b7e3afefa4d3a923320304395dc7e64ded9
Aleksandr Neklyudov <162669909+Shure0407@users.noreply.github.com>
2026-03-30 12:26:43 +0400

    Add files via upload

--- src/masks.py
+++ src/masks.py
@@ -12,8 +12,6 @@ masks_logger.setLevel(logging.DEBUG)
 
 
 def get_mask_card_number(account_or_card: Any) -> Any:
-
-
     """Функция получения номера карты владельца и выдача его с маской"""
 
     if len(account_or_card) == 16 and account_or_card.isdigit():
@@ -25,11 +23,7 @@ def get_mask_card_number(account_or_card: Any) -> Any:
         return print("Неправильно набран номер карты")
 
 
-
 def get_mask_account(account_or_card: Any) -> Any:
-
-
-
     """Функция получения счета карты владельца и выдача его с маской"""
 
     if len(account_or_card) == 20 and account_or_card.isdigit():
